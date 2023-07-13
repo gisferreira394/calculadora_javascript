3131ab66713753a53176db0dd08f18e831bab6fb
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-07-12 22:11:02 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-function valores(valor){
+var calculo = "";
+var valor1 = 0;
+
+function Valores(valor){
     
-    resultado = document.getElementByld('resultado').innerHTML
-    document.getElementByld('resultado').innerHTML += valor
+    resultado = document.getElementByld("resultado").innerHTML
+    document.getElementByld("resultado").innerHTML += valor
 }
 
 function operadores(operador){
+calculo = operador
+valor1 = document.getElementById('resultado').innerHTML
+document.getElementById('resultado').innerHTML = ""
+    }
 
+    function Calcular(){
+ valor2 = document.getElementById('resultado').innerHTML
+if(calculo == '+'){
+    total = parseint(valor1) + parseint(valor2)
+} else if (calculo == '-'){
+    total = parseint(valor1) - parseint(valor2)
+} else if (calculo == '*'){
+    total = parseint(valor1) * parseint(valor2)
+}
+else if (calculo == '/'){
+    total = parseint(valor1) / parseint(valor2)
+}
 
-    
+document.getElementById('resultado').innerHTML = total
+
+}
+
+function limpar(){
+document.getElementById('resultado').innerHTML = ""
+valor1=""
+calculo = ""
+}
+
+function C(){
+ resultado = document.getElementById('resultado').innerHTML
+
+ resultadoArray = resultado.split('')
+
+ console.log(resultadoArray)
 }
