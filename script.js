a99138e0263e0c030c40b4db12d04d79cd38a764
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-07-12 21:02:23 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+function valores(valor){
+    
+    resultado = document.getElementByld('resultado').innerHTML
+    document.getElementByld('resultado').innerHTML += valor
+}
+
+function operadores(operador){
+
+
+    
+}
